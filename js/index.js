6780d20d54edad1d01b2fbf328b13ac869786615
jayvetintec <jaylinease@yahoo.com>
2021-08-09 22:04:59 -0500

    week 3 HW

--- js/index.js
+++ js/index.js
@@ -35,22 +35,31 @@ function shuffle(array) {
 function startGame() {
   // Invoke shuffle function and store in variable
   const shuffledDeck = shuffle(deckCards);
+
   // Implement a for loop on the shuffledDeck array
+  for(let i=0; i < shuffledDeck.length; i++) {
   
     // Create the <td> tags and assign it to a variable called tdTag
+  let tdTag = document.createElement("td");
     
     // Give tdTag Element a class of card
+    tdTag.classList.add("card");
     
     // Create the <img> tag and assign it to an addImage variable
+    let addImage = document.createElement("img");
     
     // make the addImage a child of the tdTag
+    tdTag.appendChild(addImage);
     
     // Set the addImage element src path with the shuffled deck
     // TODO: replace the REPLACE ME string with the element in the shuffledDeck array at index i
-    addImage.setAttribute('src', 'img/' + 'REPLACE ME with the element in shuffleDeck at index i');
+    addImage.setAttribute('src', 'img/' + shuffledDeck[i]);
+
     // Add an alt tag to the addImage element
     addImage.setAttribute('alt', 'image of vault boy from fallout');
     // make the tdTag element a child of the deck element
+    deck.appendChild(tdTag)
+  }
     
 }
 
@@ -134,17 +143,26 @@ function compareTwo() {
   // TODO: implement
   // if the opened array has a length of two && the element at index = 0 src string
   // equals the element at index 1 src string
+  if(opened.length == 2 && opened[0].src === opened[1],src){
+
+  
   // the image srcs match
   
     // TODO: Invoke the displayMatchingCards()
-    // TODO: console log "It's a Match!"  
-    
-    
+    displayMatchingCards();
+    // TODO: console log "It's a Match!" 
+    console.log("It's a Match!");
+  }
+  
+  
   // TODO: if the image src's do not match
+  else if(opened.length == 2 && opened[0].src !== opened[1],src){
   
     // TODO: invoke the displayNotMatchingCards()
+    displayNotMatchingCards();
     // TODO: console log "No Match!"
-  
+    console.log("No Match!");
+  }
 }
 
 // TODO:
@@ -152,17 +170,23 @@ function displayMatchingCards() {
   /* Access the two cards in opened array and add
   the class of match to the imgages parent: the <li> tag
   */
+ 
   setTimeout(function() {
     // add the match class (Why are we adding it to the parentElement?)
       // the match class should make the img visible
     opened[0].parentElement.classList.add("match");
     opened[1].parentElement.classList.add("match");
+  
     // TODO: Push the flipped cards (opened[0] and opened[1]) to the matched array
-    
+    matched.push(opened[0]);
+    matched.push(opened[1]);
+
+   
     // Allow for further mouse clicks on cards
     document.body.style.pointerEvents = "auto";
     // TODO: invoke the checkIsGameFinished function
-    
+    checkIsGameFinished();
+   
    
     // Clear the opened array
     opened = [];
@@ -170,17 +194,22 @@ function displayMatchingCards() {
   // Call movesCounter to increment by one
   incrMovesCounter();
   adjustStarRating();
+
 }
 
 function displayNotMatchingCards() {
   /* After 700 miliseconds the two cards open will have
   the class of flip removed from the images parent element <li>*/
+  if(opened.length >=2){
   setTimeout(function() {
+
     // Remove class flip on images parent element
     opened[0].parentElement.classList.remove("flip");
     opened[1].parentElement.classList.remove("flip");
+
     // Allow further mouse clicks on cards
     document.body.style.pointerEvents = "auto";
+
     // Remove the cards from opened array
     opened = [];
   }, 700);
@@ -188,7 +217,7 @@ function displayNotMatchingCards() {
   incrMovesCounter();
   adjustStarRating();
 }
-
+}
 function addStatsToModal() {
   // Access the modal content div
   const statsParent = document.querySelector(".modal-content");
@@ -196,37 +225,44 @@ function addStatsToModal() {
   for (let i = 1; i <= 3; i++) {
     // Create a new Paragraph
     // TODO: create p tag and assign it a newly created statsElement variable
-    
+    const statsElement = document.createElement('p')
+
     // Add a class to the new Paragraph
     // TODO: add the stats class to the statsElement
-    
+    statsElement.classList.add('stats')
     
     // Add the new created <p> tag to the modal content
     // TODO: add the statsElement as a child of the statsParent element
-    
+    statsParent.appendChild(statsElement);
   }
   // Select all p tags with the class of stats and update the content
   let p = statsParent.querySelectorAll("p.stats");
   // Set the new <p> to have the content of stats (time, moves and star rating)
   // TODO: Update all of the innerHTML text appropriately
   p[0].innerHTML = "Update the time here with the minutes and seconds";
+  // document.getElementById("timeCounter").innerHTML = "Your time";
   p[1].innerHTML = "Update this with how many moves it took";
-  p[2].innerHTML = "Update this with the star rating";
+  p[2].innerHTML = "You have +  stars!";
+  // document.getElementById("timeCounter").innerHTML = "Your time";
 }
 
 // TODO: Implement the pseudocode
 function displayModal() {
 // use getElementByID to grab the id="close" element and assign it to a variable called modalClose
+const modalClose = document.getElementById("close");
 
 // use getElementByID to grab the id="modal" element and assign it to a variable called modal
+const modal = document.getElementById("modal");
 
 // Set modal to display block to show it
-
+modal.style.display = "block"
 
 // When the user clicks on the modalClose <span> (x), 
 modalClose.onclick = function() {
+
     // set modal to diplay none
-    
+ modal.style.diplay = "none"
+
 };
 // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
@@ -244,13 +280,16 @@ function checkIsGameFinished() {
   if (matched.length === 16) {
     // stop the game
     //TODO: invoke the stopTime function
+    stopTime();
     
     // tally stats
     // TODO: invoke the addStatsToModal
+    addStatsToModal();
     
     
     // display modal
     // TODO: invoke the displayModal function
+    displayModal()
     
     
   }
